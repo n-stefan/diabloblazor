7cb8febcbb2034fddf9d85fccc3195b269a12d86
Stefan Negulescu <tarthedark@gmail.com>
2019-11-21 23:29:31 +0200

    Change service worker behavior: network first, cache second

--- wwwroot/service-worker.ts
+++ wwwroot/service-worker.ts
@@ -1,90 +1,103 @@
 ﻿
+const serviceWorker: any = self; //as unknown as ServiceWorkerGlobalScope;
 const cacheName: string = 'Diablo';
 const gitHubPagesHostname: string = 'n-stefan.github.io';
+let handleAsOfflineUntil: number = 0;
 
-self.addEventListener('install', async event => {
+serviceWorker.addEventListener('install', async (event: any /*InstallEvent*/) => {
     //No PWA offline capability for GitHub Pages as initial caching takes too long
-    if (self.location.hostname === gitHubPagesHostname) {
-        console.log('Installing service worker...');
-        (self as any).skipWaiting();
+    if (serviceWorker.location.hostname === gitHubPagesHostname) {
+        console.info('Installing service worker...');
+        serviceWorker.skipWaiting();
     } else {
-        console.log('Installing service worker and populating cache...');
-        (event as any).waitUntil(caches.open(cacheName).then(cache => {
-            return cache.addAll([
-                '/',
-                'dist/diablo.min.css',
-                'dist/diablo.min.js',
-                'dist/diablo.js',
-                'dist/external.min.css',
-                'dist/external.min.js',
-                'dist/diabloheavy.ttf',
-                'dist/favicon.ico',
-                'dist/icon-192.png',
-                'dist/icon-512.png',
-                'dist/appconfig.json',
-                'manifest.json',
-                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
-                '_framework/_bin/Microsoft.AspNetCore.Authorization.dll',
-                '_framework/_bin/Microsoft.AspNetCore.Blazor.HttpClient.dll',
-                '_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
-                '_framework/_bin/Microsoft.AspNetCore.Components.Forms.dll',
-                '_framework/_bin/Microsoft.AspNetCore.Components.Web.dll',
-                '_framework/_bin/Microsoft.AspNetCore.Components.dll',
-                '_framework/_bin/Microsoft.AspNetCore.Metadata.dll',
-                '_framework/_bin/Microsoft.Bcl.AsyncInterfaces.dll',
-                '_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
-                '_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
-                '_framework/_bin/Microsoft.Extensions.Logging.Abstractions.dll',
-                '_framework/_bin/Microsoft.Extensions.Options.dll',
-                '_framework/_bin/Microsoft.Extensions.Primitives.dll',
-                '_framework/_bin/Microsoft.JSInterop.dll',
-                '_framework/_bin/Mono.Security.dll',
-                '_framework/_bin/Mono.WebAssembly.Interop.dll',
-                '_framework/_bin/System.Buffers.dll',
-                '_framework/_bin/System.ComponentModel.Annotations.dll',
-                '_framework/_bin/System.Core.dll',
-                '_framework/_bin/System.Memory.dll',
-                '_framework/_bin/System.Net.Http.dll',
-                '_framework/_bin/System.Numerics.Vectors.dll',
-                '_framework/_bin/System.Runtime.CompilerServices.Unsafe.dll',
-                '_framework/_bin/System.Text.Encodings.Web.dll',
-                '_framework/_bin/System.Text.Json.dll',
-                '_framework/_bin/System.Threading.Tasks.Extensions.dll',
-                '_framework/_bin/System.dll',
-                '_framework/_bin/diabloblazor.dll',
-                '_framework/_bin/mscorlib.dll',
-                '_framework/blazor.boot.json',
-                '_framework/blazor.webassembly.js',
-                '_framework/wasm/mono.js',
-                '_framework/wasm/mono.wasm',
-                'spawn.mpq', //25MB
-                'Diablo.wasm',
-                'DiabloSpawn.wasm'
-            ]);
-        }));
+        console.info('Installing service worker and populating cache...');
+        await Promise.all((await caches.keys()).map(key => caches.delete(key)));
+        await (await caches.open(cacheName)).addAll([
+            '/',
+            'dist/diablo.min.css',
+            'dist/diablo.min.js',
+            'dist/diablo.js',
+            'dist/external.min.css',
+            'dist/external.min.js',
+            'dist/diabloheavy.ttf',
+            'dist/favicon.ico',
+            'dist/icon-192.png',
+            'dist/icon-512.png',
+            'dist/appconfig.json',
+            'manifest.json',
+            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
+            '_framework/_bin/Microsoft.AspNetCore.Authorization.dll',
+            '_framework/_bin/Microsoft.AspNetCore.Blazor.HttpClient.dll',
+            '_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
+            '_framework/_bin/Microsoft.AspNetCore.Components.Forms.dll',
+            '_framework/_bin/Microsoft.AspNetCore.Components.Web.dll',
+            '_framework/_bin/Microsoft.AspNetCore.Components.dll',
+            '_framework/_bin/Microsoft.AspNetCore.Metadata.dll',
+            '_framework/_bin/Microsoft.Bcl.AsyncInterfaces.dll',
+            '_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
+            '_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
+            '_framework/_bin/Microsoft.Extensions.Logging.Abstractions.dll',
+            '_framework/_bin/Microsoft.Extensions.Options.dll',
+            '_framework/_bin/Microsoft.Extensions.Primitives.dll',
+            '_framework/_bin/Microsoft.JSInterop.dll',
+            '_framework/_bin/Mono.Security.dll',
+            '_framework/_bin/Mono.WebAssembly.Interop.dll',
+            '_framework/_bin/System.Buffers.dll',
+            '_framework/_bin/System.ComponentModel.Annotations.dll',
+            '_framework/_bin/System.Core.dll',
+            '_framework/_bin/System.Memory.dll',
+            '_framework/_bin/System.Net.Http.dll',
+            '_framework/_bin/System.Numerics.Vectors.dll',
+            '_framework/_bin/System.Runtime.CompilerServices.Unsafe.dll',
+            '_framework/_bin/System.Text.Encodings.Web.dll',
+            '_framework/_bin/System.Text.Json.dll',
+            '_framework/_bin/System.Threading.Tasks.Extensions.dll',
+            '_framework/_bin/System.dll',
+            '_framework/_bin/diabloblazor.dll',
+            '_framework/_bin/mscorlib.dll',
+            '_framework/blazor.boot.json',
+            '_framework/blazor.webassembly.js',
+            '_framework/wasm/mono.js',
+            '_framework/wasm/mono.wasm',
+            'spawn.mpq', //25MB
+            'Diablo.wasm',
+            'DiabloSpawn.wasm'
+        ]);
+        serviceWorker.skipWaiting();
     }
 });
 
-self.addEventListener('fetch', (event: any) => {
+serviceWorker.addEventListener('fetch', (event: any /*FetchEvent*/): Response => {
+    if (event.request.method !== 'GET')
+        return;
+
     //No PWA offline capability for GitHub Pages as initial caching takes too long
-    if (self.location.hostname === gitHubPagesHostname) {
+    if (serviceWorker.location.hostname === gitHubPagesHostname)
         return null;
-    } else {
-        event.respondWith(caches.match(event.request).then(cacheResponse => {
-            if (cacheResponse) {
-                console.log(`From the offline cache: ${event.request.url}`);
-                return cacheResponse;
-            }
-            else return fetch(event.request).then(netResponse => {
-                const responseClone = netResponse.clone();
-                caches.open(cacheName).then(cache => cache.put(event.request, responseClone));
-                console.log(`From the internet: ${event.request.url}`);
-                return netResponse;
-            }).catch(() => {
-                console.error(`Could not fetch ${event.request.url} from either the offline cache or the internet!`);
-                return null;
-            })
-        }));
-    }
+    else
+        event.respondWith(getFromNetworkOrCache(event.request));
 });
+  
+async function getFromNetworkOrCache(request: Request) {
+    if (new Date().valueOf() > handleAsOfflineUntil) {
+        try {
+            const networkResponse = await fetchWithTimeout(request, 1000);
+            (await caches.open(cacheName)).put(request, networkResponse.clone());
+            console.info(`From the network: ${request.url}`);
+            return networkResponse;
+        } catch (ex) {
+            handleAsOfflineUntil = new Date().valueOf() + 3000;
+        }
+    }
+
+    console.info(`From the offline cache: ${request.url}`);
+    return caches.match(request);
+}
+
+function fetchWithTimeout(request: Request, timeout: number): Promise<Response> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => reject('Fetch timed out'), timeout);
+        fetch(request).then(resolve, reject);
+    });
+}
