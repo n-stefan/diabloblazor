9129a54a7f4bd39127eb8f6f8e36557c096045ab
Stefan Negulescu <tarthedark@gmail.com>
2020-01-25 18:22:43 +0200

    Free GCHandles eagerly

--- wwwroot/scripts/helper.ts
+++ wwwroot/scripts/helper.ts
@@ -2,11 +2,11 @@
 declare const base64js: any;
 
 class Helper {
-    public static fromBase64ToUint8Array = (base64: string): Uint8Array => {
-        //new Uint8Array(atob(base64).split('').map(c => c.charCodeAt(0)))
-        //return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
-        return base64js.toByteArray(base64);
-    }
+    //public static fromBase64ToUint8Array = (base64: string): Uint8Array => {
+    //    //new Uint8Array(atob(base64).split('').map(c => c.charCodeAt(0)))
+    //    //return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+    //    return base64js.toByteArray(base64);
+    //}
 
     public static fromUint8ArrayToBase64 = (array: Uint8Array): string => {
         return base64js.fromByteArray(array);
