9a99f03773def846cebf85ba92e5fbee5ebb7eb8
Stefan Negulescu <tarthedark@gmail.com>
2020-05-11 22:34:45 +0300

    Attempt to deploy assets precompressed with Brotli

--- service-worker.js
+++ service-worker.js
@@ -11,6 +11,9 @@ serviceWorker.addEventListener('install', async (event) => {
         console.info('Installing service worker and populating cache...');
         await Promise.all((await caches.keys()).map(key => caches.delete(key)));
         await (await caches.open(cacheName)).addAll([
+            '_framework/blazor.boot.json',
+            '_framework/blazor.webassembly.js',
+            '_framework/wasm/dotnet.3.2.0-rc1.20222.2.js',
             '/',
             'dist/diablo.min.css',
             'dist/diablo.min.js',
@@ -21,37 +24,10 @@ serviceWorker.addEventListener('install', async (event) => {
             'dist/favicon.ico',
             'dist/icon-192.png',
             'dist/icon-512.png',
-            'dist/appconfig.json',
+            'appsettings.json',
             'manifest.json',
             'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
             'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
-            '_framework/_bin/diabloblazor.dll',
-            '_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
-            '_framework/_bin/Microsoft.AspNetCore.Blazor.HttpClient.dll',
-            '_framework/_bin/Microsoft.AspNetCore.Components.dll',
-            '_framework/_bin/Microsoft.AspNetCore.Components.Web.dll',
-            '_framework/_bin/Microsoft.Bcl.AsyncInterfaces.dll',
-            '_framework/_bin/Microsoft.Extensions.Configuration.Abstractions.dll',
-            '_framework/_bin/Microsoft.Extensions.Configuration.dll',
-            '_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
-            '_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
-            '_framework/_bin/Microsoft.Extensions.Logging.Abstractions.dll',
-            '_framework/_bin/Microsoft.Extensions.Primitives.dll',
-            '_framework/_bin/Microsoft.JSInterop.dll',
-            '_framework/_bin/Mono.WebAssembly.Interop.dll',
-            '_framework/_bin/mscorlib.dll',
-            '_framework/_bin/System.Core.dll',
-            '_framework/_bin/System.dll',
-            '_framework/_bin/System.Net.Http.dll',
-            '_framework/_bin/System.Runtime.CompilerServices.Unsafe.dll',
-            '_framework/_bin/System.Text.Encodings.Web.dll',
-            '_framework/_bin/System.Text.Json.dll',
-            '_framework/_bin/WebAssembly.Bindings.dll',
-            '_framework/_bin/WebAssembly.Net.Http.dll',
-            '_framework/blazor.boot.json',
-            '_framework/blazor.webassembly.js',
-            '_framework/wasm/dotnet.js',
-            '_framework/wasm/dotnet.wasm',
             'spawn.mpq',
             'Diablo.wasm',
             'DiabloSpawn.wasm'
@@ -71,7 +47,6 @@ async function getFromNetworkOrCache(request) {
     if (new Date().valueOf() > handleAsOfflineUntil) {
         try {
             const networkResponse = await fetchWithTimeout(request, 1000);
-            (await caches.open(cacheName)).put(request, networkResponse.clone());
             console.info(`From the network: ${request.url}`);
             return networkResponse;
         }
