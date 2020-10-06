ecde200571afc4884472e935bdf767296469f604
Stefan Negulescu <tarthedark@gmail.com>
2020-10-06 20:30:28 +0300

    Improve service worker

--- wwwroot/service-worker.ts
+++ wwwroot/service-worker.ts
@@ -1,74 +1,65 @@
 ﻿
-const serviceWorker: any = self; //as unknown as ServiceWorkerGlobalScope;
+self.addEventListener('install', (event: any) => event.waitUntil(onInstall(event)));
+self.addEventListener('fetch', (event: any) => event.respondWith(onFetch(event)));
+
 const cacheName: string = 'DiabloOfflineCache';
 const gitHubPagesHostname: string = 'n-stefan.github.io';
-let handleAsOfflineUntil: number = 0;
 
-serviceWorker.addEventListener('install', async (event: any /*InstallEvent*/) => {
+async function onInstall(event: any): Promise<void> {
     //No PWA offline capability for GitHub Pages as initial caching takes too long
-    if (serviceWorker.location.hostname === gitHubPagesHostname) {
-        console.info('Installing service worker...');
-        serviceWorker.skipWaiting();
-    } else {
-        console.info('Installing service worker and populating cache...');
-        await Promise.all((await caches.keys()).map(key => caches.delete(key)));
-        await (await caches.open(cacheName)).addAll([
-            '_framework/blazor.boot.json',
-            '_framework/blazor.webassembly.js',
-            '_framework/dotnet.5.0.0-rc.1.20451.14.js',
-            '/',
-            'dist/brotli.decode.min.js',
-            'dist/external.min.css',
-            'dist/external.min.js',
-            'dist/diablo.min.css',
-            'dist/diablo.min.js',
-            'dist/diablo.js',
-            'dist/diabloheavy.ttf',
-            'dist/favicon.ico',
-            'dist/icon-192.png',
-            'dist/icon-512.png',
-            'manifest.json',
-            'appsettings.json',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
-            'spawn.mpq', //25MB
-            'Diablo.wasm',
-            'DiabloSpawn.wasm'
-        ]);
-        serviceWorker.skipWaiting();
-    }
-});
+    //if (self.location.hostname === gitHubPagesHostname) return;
 
-serviceWorker.addEventListener('fetch', (event: any /*FetchEvent*/): Response => {
-    if (event.request.method !== 'GET')
-        return;
+    console.info('Installing service worker');
 
-    //No PWA offline capability for GitHub Pages as initial caching takes too long
-    if (serviceWorker.location.hostname === gitHubPagesHostname)
-        return null;
-    else
-        event.respondWith(getFromNetworkOrCache(event.request));
-});
-  
-async function getFromNetworkOrCache(request: Request) {
-    if (new Date().valueOf() > handleAsOfflineUntil) {
-        try {
-            const networkResponse = await fetchWithTimeout(request, 1000);
-            //(await caches.open(cacheName)).put(request, networkResponse.clone());
-            console.info(`From the network: ${request.url}`);
-            return networkResponse;
-        } catch (ex) {
-            handleAsOfflineUntil = new Date().valueOf() + 3000;
-        }
-    }
+    await caches.delete(cacheName);
 
-    console.info(`From the offline cache: ${request.url}`);
-    return caches.match(request);
+    await caches.open(cacheName).then(cache => cache.addAll([
+        '_framework/blazor.boot.json',
+        '_framework/blazor.webassembly.js',
+        '_framework/dotnet.5.0.0-rc.1.20451.14.js',
+        '/',
+        'dist/brotli.decode.min.js',
+        'dist/external.min.css',
+        'dist/external.min.js',
+        'dist/diablo.min.css',
+        'dist/diablo.min.js',
+        'dist/diablo.js',
+        'dist/diabloheavy.ttf',
+        'dist/favicon.ico',
+        'dist/icon-192.png',
+        'dist/icon-512.png',
+        'manifest.json',
+        'appsettings.json',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
+        //'spawn.mpq', //25MB
+        'Diablo.wasm',
+        'DiabloSpawn.wasm'
+    ]));
 }
 
-function fetchWithTimeout(request: Request, timeout: number): Promise<Response> {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject('Fetch timed out'), timeout);
-        fetch(request).then(resolve, reject);
-    });
+async function onFetch(event: any): Promise<Response> {
+    //No PWA offline capability for GitHub Pages as initial caching takes too long
+    //if (self.location.hostname === gitHubPagesHostname) return null;
+
+    if (event.request.method !== 'GET') return null;
+
+    try {
+        const fromNetwork = await fetch(event.request);
+        if (fromNetwork) {
+            console.info(`From the network: ${event.request.url}`);
+            return fromNetwork;
+        }
+    } catch (e) {
+        const shouldServeIndexHtml = event.request.mode === 'navigate';
+        const request = shouldServeIndexHtml ? '/' : event.request;
+        const cache = await caches.open(cacheName);
+        const fromCache = await cache.match(request);
+        if (fromCache) {
+            console.info(`From the offline cache: ${event.request.url}`);
+            return fromCache;
+        }
+        console.error(`Couldn't fetch ${event.request.url} from either the network or the offline cache`);
+        return null;
+    }
 }
