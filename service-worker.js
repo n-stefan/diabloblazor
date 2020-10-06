788cbbca91b7f257c2430922bc90d21081337973
Stefan Negulescu <tarthedark@gmail.com>
2020-10-06 21:01:42 +0300

    Updates from master

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,54 @@
-const serviceWorker = self;
+self.addEventListener('install', (event) => event.waitUntil(onInstall(event)));
+self.addEventListener('fetch', (event) => event.respondWith(onFetch(event)));
 const cacheName = 'DiabloOfflineCache';
 const gitHubPagesHostname = 'n-stefan.github.io';
-let handleAsOfflineUntil = 0;
-serviceWorker.addEventListener('install', async (event) => {
-    if (serviceWorker.location.hostname === gitHubPagesHostname) {
-        console.info('Installing service worker...');
-        serviceWorker.skipWaiting();
-    }
-    else {
-        console.info('Installing service worker and populating cache...');
-        await Promise.all((await caches.keys()).map(key => caches.delete(key)));
-        await (await caches.open(cacheName)).addAll([
-            '_framework/blazor.boot.json',
-            '_framework/blazor.webassembly.js',
-            '_framework/dotnet.5.0.0-rc.1.20451.14.js',
-            '/',
-            'dist/brotli.decode.min.js',
-            'dist/external.min.css',
-            'dist/external.min.js',
-            'dist/diablo.min.css',
-            'dist/diablo.min.js',
-            'dist/diablo.js',
-            'dist/diabloheavy.ttf',
-            'dist/favicon.ico',
-            'dist/icon-192.png',
-            'dist/icon-512.png',
-            'manifest.json',
-            'appsettings.json',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
-            'spawn.mpq',
-            'Diablo.wasm',
-            'DiabloSpawn.wasm'
-        ]);
-        serviceWorker.skipWaiting();
-    }
-});
-serviceWorker.addEventListener('fetch', (event) => {
-    if (event.request.method !== 'GET')
-        return;
-    if (serviceWorker.location.hostname === gitHubPagesHostname)
-        return null;
-    else
-        event.respondWith(getFromNetworkOrCache(event.request));
-});
-async function getFromNetworkOrCache(request) {
-    if (new Date().valueOf() > handleAsOfflineUntil) {
-        try {
-            const networkResponse = await fetchWithTimeout(request, 1000);
-            console.info(`From the network: ${request.url}`);
-            return networkResponse;
-        }
-        catch (ex) {
-            handleAsOfflineUntil = new Date().valueOf() + 3000;
-        }
-    }
-    console.info(`From the offline cache: ${request.url}`);
-    return caches.match(request);
+async function onInstall(event) {
+    console.info('Installing service worker');
+    await caches.delete(cacheName);
+    await caches.open(cacheName).then(cache => cache.addAll([
+        '_framework/blazor.boot.json',
+        '_framework/blazor.webassembly.js',
+        '_framework/dotnet.5.0.0-rc.1.20451.14.js',
+        '/',
+        'dist/brotli.decode.min.js',
+        'dist/external.min.css',
+        'dist/external.min.js',
+        'dist/diablo.min.css',
+        'dist/diablo.min.js',
+        'dist/diablo.js',
+        'dist/diabloheavy.ttf',
+        'dist/favicon.ico',
+        'dist/icon-192.png',
+        'dist/icon-512.png',
+        'manifest.json',
+        'appsettings.json',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
+        'Diablo.wasm',
+        'DiabloSpawn.wasm'
+    ]));
 }
-function fetchWithTimeout(request, timeout) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject('Fetch timed out'), timeout);
-        fetch(request).then(resolve, reject);
-    });
+async function onFetch(event) {
+    if (event.request.method !== 'GET')
+        return null;
+    try {
+        const fromNetwork = await fetch(event.request);
+        if (fromNetwork) {
+            console.info(`From the network: ${event.request.url}`);
+            return fromNetwork;
+        }
+    }
+    catch (e) {
+        const shouldServeIndexHtml = event.request.mode === 'navigate';
+        const request = shouldServeIndexHtml ? '/' : event.request;
+        const cache = await caches.open(cacheName);
+        const fromCache = await cache.match(request);
+        if (fromCache) {
+            console.info(`From the offline cache: ${event.request.url}`);
+            return fromCache;
+        }
+        console.error(`Couldn't fetch ${event.request.url} from either the network or the offline cache`);
+        return null;
+    }
 }
 //# sourceMappingURL=service-worker.js.map
