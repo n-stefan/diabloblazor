419643e205d53cc8e815f99fc9ae9cf0def17c7e
Stefan Negulescu <tarthedark@gmail.com>
2021-06-01 16:14:12 +0300

    Use top level statements

--- Program.cs
+++ Program.cs
@@ -1,37 +1,28 @@
-﻿using diabloblazor.Services;
+﻿using diabloblazor;
+using diabloblazor.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace diabloblazor
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<AppState>();
-            builder.Services.AddScoped<Interop>();
-            builder.Services.AddScoped<Worker>();
-            builder.Services.AddSingleton<ExceptionHandler>();
-            //builder.Services.AddIndexedDB(db =>
-            //{
-            //    db.DbName = "diablo_fs";
-            //    db.Version = 1;
-            //    db.Stores.Add(new StoreSchema
-            //    {
-            //        Name = "kv",
-            //        PrimaryKey = new IndexSpec { KeyPath = "Name", Name = "Name", Auto = false }
-            //    });
-            //});
+builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AppState>();
+builder.Services.AddScoped<Interop>();
+builder.Services.AddScoped<Worker>();
+builder.Services.AddSingleton<ExceptionHandler>();
+//builder.Services.AddIndexedDB(db =>
+//{
+//    db.DbName = "diablo_fs";
+//    db.Version = 1;
+//    db.Stores.Add(new StoreSchema
+//    {
+//        Name = "kv",
+//        PrimaryKey = new IndexSpec { KeyPath = "Name", Name = "Name", Auto = false }
+//    });
+//});
 
-            builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<App>("app");
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
