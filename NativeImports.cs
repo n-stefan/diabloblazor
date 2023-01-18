3c8ac2c15f6ba3c9bc119c467a013714c88b238f
Stefan Negulescu <tarthedark@gmail.com>
2023-01-18 19:25:04 +0200

    Use LibraryImport to generate P/Invoke marshalling code at compile time

--- NativeImports.cs
+++ NativeImports.cs
@@ -1,21 +1,21 @@
 ﻿namespace diabloblazor;
 
-static class NativeImports
+static partial class NativeImports
 {
     private const string main = "main.cpp";
 
-    [DllImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-    internal static extern void DApi_Init(uint time, int offscreen, int v0, int v1, int v2, int spawn, IntPtr[] callbacks);
+    [LibraryImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial void DApi_Init(uint time, int offscreen, int v0, int v1, int v2, int spawn, IntPtr[] callbacks);
 
-    [DllImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-    internal static extern void DApi_Mouse(int action, int button, int mods, int x, int y);
+    [LibraryImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial void DApi_Mouse(int action, int button, int mods, int x, int y);
 
-    [DllImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-    internal static extern void DApi_Key(int action, int mods, int key);
+    [LibraryImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial void DApi_Key(int action, int mods, int key);
 
-    [DllImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-    internal static extern void DApi_Char(int chr);
+    [LibraryImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial void DApi_Char(int chr);
 
-    [DllImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-    internal static extern void DApi_Render(uint time);
+    [LibraryImport(main), DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
+    internal static partial void DApi_Render(uint time);
 }
